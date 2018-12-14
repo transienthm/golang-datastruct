07c96eabc60ff0ead2dda143e20764e9a113e46e
wangbin58 <wangbin58@meituan.com>
2018-12-14 20:32:15 +0800

    linkedlist

--- src/linkedlist/LinkedList.go
+++ src/linkedlist/LinkedList.go
@@ -42,3 +42,32 @@ func (head *Node) Traverse() {
 	fmt.Println("-------done--------")
 }
 
+func (head *Node) Get(i int) Elem {
+	p := head.Next
+	for j := 1; j < i; j++ {
+		if nil == p {
+			return -1
+		}
+		p = p.Next
+	}
+
+	return p.Date
+}
+
+func (head *Node) Delete(i int) bool {
+	p := head
+	j := 1
+	for nil != p && j < i {
+		p = p.Next
+		j++
+	}
+
+	if nil == p || j > i {
+		fmt.Println("pls check i: ", i)
+		return false
+	}
+
+	p.Next = p.Next.Next
+	return true
+}
+
